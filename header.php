c6fa12e7a666cdadf2c3048c14e682286701c26b
John Miehler <kanzlei@ra-miehler.de>
2017-01-10 09:41:23 +0100

    Änderungen Footer

--- header.php
+++ header.php
@@ -67,7 +67,7 @@ if ( defined( 'WPSEO_VERSION' ) ) {
     <?php get_sidebar('top'); ?>
 				<?php wp_nav_menu(array(
 				    'container'       => 'div',
-						'container_class'	=> 'main-nav dunkel',
+						'container_class'	=> 'main-nav dunkel-verlauf',
 						'fallback_cb'	  =>  'responsive_fallback_menu',
 						'theme_location'  => 'header-menu')
 					); 
